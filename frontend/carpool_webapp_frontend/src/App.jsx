c8d5a9a7158fed12bd29defacb04a141a9e8557b
Edward Arzoumanov <edwardarzumanov@gmail.com>
2025-03-09 14:59:13 -0400

    Make prototype sign up page

--- frontend/carpool_webapp_frontend/src/App.jsx
+++ frontend/carpool_webapp_frontend/src/App.jsx
@@ -4,31 +4,58 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // for storing our form data when signing up
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  // for storing input changes to form data
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      const data = await response.json();
+      console.log(`response is:`, data);
+      if (response.ok) {
+        console.log(`Success! Returned token is: ${data.access_token}`);
+      }
+      else {
+        console.log(`Error: ${data.error}`);
+      }
+    }
+    catch (error) {
+      console.log(`Sign up request failed: ${error.message}`)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="App">
+      <h1>Carpool Coordinator Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required>
+        </input>
+        <input type="text" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required>
+        </input>
+        <input type="text" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required>
+        </input>
+        <button type="submit">
+          Sign Up
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </div >
   )
 }
 
